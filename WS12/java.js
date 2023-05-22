3eee68e2137cf0f8d2e18fb977488ba567d0b6a4
ONNixd <onni.jansson@gmail.com>
2023-05-22 22:40:09 +0300

    W12 Nettisivu

---
+++ WS12/java.js
@@ -0,0 +1,14 @@
+/* Navbar nappulan toiminto */
+function Funktio() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+
+
